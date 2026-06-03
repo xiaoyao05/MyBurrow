7dfc62e315040ff1c7c9810e4d2a9480159981ad
Wong Xiao Yao <wongxy0916@gmail.com>
2026-06-04 00:14:09 +0800

    debug

--- frontend/src/httpClient.jsx
+++ frontend/src/httpClient.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
+console.log("API URL:", API_URL);
+
 const instance = axios.create({ 
   baseURL: API_URL,
   withCredentials: true });
