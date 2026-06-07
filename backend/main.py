ab7c6488cfc4a3458816c2dced0a828c0f5f905f
Wong Xiao Yao <wongxy0916@gmail.com>
2026-06-07 21:04:16 +0800

    added config

--- backend/main.py
+++ backend/main.py
@@ -119,7 +119,7 @@ app = FastAPI(lifespan=lifespan)
 default_allowed_origins = [
     "http://localhost:5173",
     "https://myburrow.vercel.app",
-    "https://myburrow-9twk58ftm-xiaoyao05s-projects.vercel.app",
+    "https://myburrow-l791u04ru-xiaoyao05s-projects.vercel.app",
 ]
 
 app.add_middleware(
