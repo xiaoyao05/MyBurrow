8a566833042bda0ae9163c2e65134eff38f55fa4
Wong Xiao Yao <wongxy0916@gmail.com>
2026-06-19 00:19:05 +0800

    allow cors

--- backend/main.py
+++ backend/main.py
@@ -119,7 +119,7 @@ app = FastAPI(lifespan=lifespan)
 default_allowed_origins = [
     "http://localhost:5173",
     "https://myburrow.vercel.app",
-    "https://myburrow-kyet81h6c-xiaoyao05s-projects.vercel.app",
+    "https://myburrow-htkxtzleu-xiaoyao05s-projects.vercel.app",
 ]
 
 app.add_middleware(
